8f30d148fd63f5fc0bf566712a4ba704135e8a04
rupalikavale <coderquill@gmail.com>
2024-03-26 14:47:45 -0400

    Part 1 : Process tasks using a single thread approach Current status: -Implemented the pipeline in a simple, single-threaded way: ---Fetched tasks and executed them ---Kept track of number of tasks processed ---Kept track of number of tasks dropped

--- main.go
+++ main.go
@@ -63,6 +63,7 @@ func main() {
 	log.Printf("Total duration: %s\n", duration)
 }
 
+// Task 1: single-threaded design will process each task one by one as they come in
 func startProcessing() (processedTasks int, droppedTasks uint64, err error) {
 	// Establish a new connection to the task queue.
 	taskQueueConn, err := lib.NewTaskQueueConn()
@@ -70,8 +71,25 @@ func startProcessing() (processedTasks int, droppedTasks uint64, err error) {
 		return 0, 0, err
 	}
 
-	// Listen to the tasks in the task queue.
+	// Listen to the task queue.
 	tasks := taskQueueConn.Listen()
-	
+
+	// Process tasks
+	for task := range tasks {
+		//If err occurred:
+		if err := task.Do(); err != nil {
+			log.Printf("Failed to process a task: %s\n", err)
+			//If tasks file had a function to retrieve a task id for a particular task,
+			//we can log that this particular task failed
+		} else {
+			log.Printf("Processed a task")
+			processedTasks++
+		}
+	}
+
+	// Retrieve the number of dropped tasks using lib function.
+	droppedTasks = taskQueueConn.Dropped()
+
+	// If no err occurred, return all the counts.
 	return processedTasks, droppedTasks, nil
 }
